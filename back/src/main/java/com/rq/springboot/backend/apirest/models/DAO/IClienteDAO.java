b75a8f299e95e09e4a92c172f246bf1a64005059
randhal <randhal.rea@gmail.com>
2019-05-04 17:03:04 -0500

    se elimino back

--- back/src/main/java/com/rq/springboot/backend/apirest/models/DAO/IClienteDAO.java
+++
@@ -1,9 +0,0 @@
-package com.rq.springboot.backend.apirest.models.DAO;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.rq.springboot.backend.apirest.models.entity.Cliente;
-
-public interface IClienteDAO extends CrudRepository<Cliente, Long>{
-	
-}
